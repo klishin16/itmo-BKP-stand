8a57e89a6b52609e5b50ae380df759aeccba3e11
n.klishin <n.klishin@trainingspace.ru>
2024-05-01 22:53:22 +0300

    Gitlab CI update 39

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,13 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieComponent } from "./movie/movie.component";
+import { NgForOf } from "@angular/common";
+
+export interface IMovie {
+  id: string;
+  name: string;
+  previewImage: string;
+  previewVideoLink: string;
+  genre: string;
+}
+
+const BACKEND_URL = 'http://backend.local.dev';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MovieComponent, NgForOf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'front-angular';
+
+  n = 128;
+  public moviesList: IMovie[] = []
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  public ngOnInit(): void {
+    fetch(`${BACKEND_URL}/movies/${this.n}`)
+      .then(response => response.json())
+      .then(data => this.moviesList = data)
+  }
+
+  public render() {
+    this.cdr.detectChanges();
+  }
 }
