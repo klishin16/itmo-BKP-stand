326d2c512ee0c152e09323e074d7e7ec7ada6774
n.klishin <n.klishin@trainingspace.ru>
2024-05-05 19:36:35 +0300

    Gitlab CI update 43

--- front-svelte/src/App.svelte
+++ front-svelte/src/App.svelte
@@ -1,47 +1,52 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { onMount } from "svelte";
+
+  interface IMovie {
+    id: string;
+    name: string;
+    previewImage: string;
+    previewVideoLink: string;
+    genre: string;
+  }
+
+  const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
+
+  const n: number = 128;
+
+  let movies: IMovie[] = [];
+
+  onMount(async () => {
+    await fetch(`${BACKEND_URL}/movies/${n}`)
+            .then(response => response.json())
+            .then(data => {
+              movies = [...movies, ...data]
+            })
+  })
+
+
+  let count = 1;
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class='page'>
+    <button id="render-btn" on:click={() => count += 1}>Render</button>
+    <h4>Count: {count}</h4>
+    <ul>
+      {#each movies as {id, name, genre, previewImage, previewVideoLink} (id)}
+        <li class="list-item">
+          <span>{ name }</span>
+          <span>{ genre }</span>
+          <span>{ previewImage }</span>
+          <span>{ previewVideoLink }</span>
+          <span class="hidden">{ count }</span>
+        </li>
+      {/each}
+    </ul>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .hidden {
+    display: none;
   }
 </style>
